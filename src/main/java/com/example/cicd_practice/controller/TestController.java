59d0a073e5d685ea1f9f793c9700bbd93de7e527
Darren4641 <74559561+Darren4641@users.noreply.github.com>
2023-03-09 14:37:42 +0900

    빌드 테스트2

--- src/main/java/com/example/cicd_practice/controller/TestController.java
+++ src/main/java/com/example/cicd_practice/controller/TestController.java
@@ -10,7 +10,7 @@ public class TestController {
 
     @RequestMapping("/test")
     public String test() {
-        return "docker-jenkins-test22";
+        return "docker-jenkins-test12412";
     }
 
 
